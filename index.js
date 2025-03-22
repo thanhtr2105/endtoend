101390d4eba969a101d5cd4f2dba96aa7fc77373
thanhtr2105 <thanhtr2105@gmail.com>
2025-03-22 00:24:19 -0700

    Update PORT

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post("/api/notes/:id", (request, response) => {
   response.json(body);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
